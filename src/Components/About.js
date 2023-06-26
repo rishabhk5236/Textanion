7e61e1f5ce8e895600d8b614e3b4d75cc59e84bb
Rishabh <rishabhk5236@gmail.com>
2023-06-26 17:54:12 +0530

    26June2023

--- src/Components/About.js
+++ src/Components/About.js
@@ -1,4 +1,6 @@
 import React from "react";
+import AboutPng from '../Resources/AboutPng.png';
+import '../CSS/About.css';
 
 export default function About(props) {
 
@@ -9,16 +11,23 @@ export default function About(props) {
     <div className="container my-3" style={darkmodestyle}>
         <h1 className="text-center">About us</h1>
         
-        <div class="card mb-3">
+        <div class="card aboutCard mb-3" style={darkmodestyle}>
   <div class="row g-0">
     <div class="col-md-4">
-      <img src="..." class="img-fluid rounded-start" alt="..."/>
+      <img src={AboutPng} class="img-fluid rounded-start aboutCardImg" alt="..."/>
     </div>
     <div class="col-md-8">
       <div class="card-body">
-        <h5 class="card-title">Card title</h5>
-        <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-        <p class="card-text"><small class="text-body-secondary">Last updated 3 mins ago</small></p>
+        <h3 class="card-title">Textanion</h3>
+        <p class="card-text">We Provide a number of servcies there in our Application , Here you can - </p>
+        <p class="card-text">1. Convert the Case of the Text</p>
+        <p class="card-text">2. Extract all Emails from the Text</p>
+        <p class="card-text">3. Encode and Decode the Text</p>
+        <p class="card-text">4. Copy Text</p>
+        <p class="card-text">5. Remove Extra Spaces From the text</p>
+        <p class="card-text"><small class="text-body-secondary">One Solution for all your text Related Problems</small></p>
+        <p class="card-text my-0"><strong class="text-body-secondary">Made By : Rishabh Kanaujiya</strong></p>
+        <p class="card-text my-0"><strong class="text-body-secondary">rishabhk5236@gmail.com</strong></p>
       </div>
     </div>
   </div>
